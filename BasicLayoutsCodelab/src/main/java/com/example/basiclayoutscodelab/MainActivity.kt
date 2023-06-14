1ca9b977a71ca5dcf5c538d8b25a390223130183
Thirawat-r <thirawat4work.info@gmail.com>
2023-06-14 14:53:28 +0700

    Create AlignYourBodyElement composable

--- BasicLayoutsCodelab/src/main/java/com/example/basiclayoutscodelab/MainActivity.kt
+++ BasicLayoutsCodelab/src/main/java/com/example/basiclayoutscodelab/MainActivity.kt
@@ -3,9 +3,17 @@ package com.example.basiclayoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -14,7 +22,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,6 +69,46 @@ fun SearchBar(
             Text(text = stringResource(id = R.string.placeholder_search))
         }
     )
+}
+
+@Composable
+fun AlignYourBodyElement(
+    modifier: Modifier,
+    @DrawableRes painterRes: Int,
+    @StringRes text: Int
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = painterRes),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape),
+        )
+        Text(
+            text = stringResource(id = text),
+            style = MaterialTheme.typography.labelLarge,
+            modifier = Modifier.paddingFromBaseline(
+                top = 24.dp, bottom = 8.dp
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlignBodyElementPreview() {
+    ComposeTutorialTheme {
+        AlignYourBodyElement(
+            modifier = Modifier.padding(all = 8.dp),
+            painterRes = R.drawable.ab1_inversions,
+            text = R.string.ab1_inversions
+        )
+    }
 }
 
 
--- BasicLayoutsCodelab/src/main/java/com/example/basiclayoutscodelab/ui/theme/Type.kt
+++ BasicLayoutsCodelab/src/main/java/com/example/basiclayoutscodelab/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.basiclayoutscodelab.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.basiclayoutscodelab.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -13,7 +15,7 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.5.sp,
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
@@ -31,4 +33,61 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+private val fontFamilyKulim = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.kulim_park_regular
+        ),
+        Font(
+            resId = R.font.kulim_park_light,
+            weight = FontWeight.Light
+        )
+    )
+)
+
+private val fontFamilyLato = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.lato_regular
+        ),
+        Font(
+            resId = R.font.lato_bold,
+            weight = FontWeight.Bold
+        )
+    )
+)
+
+val typography = Typography(
+    headlineMedium = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontWeight = FontWeight.Light,
+        fontSize = 28.sp,
+        letterSpacing = (1.15).sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontSize = 16.sp,
+        letterSpacing = (1.15).sp
+    ),
+    labelLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    titleLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = (1.15).sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = fontFamilyKulim,
+        fontSize = 12.sp,
+        letterSpacing = (1.15).sp
+    ),
 )
