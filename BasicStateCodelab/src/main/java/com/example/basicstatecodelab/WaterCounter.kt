2e7a539809b3ab227ef5e3f1afd84f251af9e1c6
Thirawat-r <thirawat4work.info@gmail.com>
2023-06-17 22:15:44 +0700

    Update WaterCounter composable function

--- BasicStateCodelab/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,8 @@
 package com.example.basicstatecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -9,9 +11,21 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(all = 16.dp)
-    )
+    Column(
+        modifier = modifier
+            .padding(all = 16.dp)
+    ) {
+        Text(
+            text = "You've had $count glasses."
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = modifier.padding(top = 8.dp)
+        ) {
+            Text(
+                text = "Add more"
+            )
+        }
+    }
 }
 
