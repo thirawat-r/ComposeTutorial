ebfded6f920b526799ba8a054c75bc77a552cd7c
Thirawat-r <thirawat4work.info@gmail.com>
2023-06-18 21:00:11 +0700

    Create WellnessTaskItem composable function

---
+++ BasicStateCodelab/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
@@ -0,0 +1,41 @@
+package com.example.basicstatecodelab
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(all = 16.dp),
+            text = taskName
+        )
+        IconButton(
+            onClick = { onClose() }
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Close,
+                contentDescription = "Close"
+            )
+        }
+    }
+}
+
