cee5e8c2ce9fc727efd877b83849c3783c0a9208
Thirawat-r <thirawat4work.info@gmail.com>
2023-06-19 15:52:59 +0700

    Create WellnessTaskItem composable function

--- BasicStateCodelab/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
+++ BasicStateCodelab/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
@@ -4,10 +4,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,6 +20,26 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WellnessTaskItem(
     taskName: String,
+    modifier: Modifier = Modifier
+) {
+    var checkedState by remember {
+        mutableStateOf(false)
+    }
+
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue },
+        onClose = { },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -27,6 +52,10 @@ fun WellnessTaskItem(
                 .weight(1f)
                 .padding(all = 16.dp),
             text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
         )
         IconButton(
             onClick = { onClose() }
