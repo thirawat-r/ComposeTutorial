1d76cc70914886a1dc6d2e5a4869f641fc08fcaf
Thirawat-r <thirawat4work.info@gmail.com>
2023-06-19 16:09:18 +0700

    Use remember saveable composable instead of remember state

--- BasicStateCodelab/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
+++ BasicStateCodelab/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,7 +22,7 @@ fun WellnessTaskItem(
     taskName: String,
     modifier: Modifier = Modifier
 ) {
-    var checkedState by remember {
+    var checkedState by rememberSaveable {
         mutableStateOf(false)
     }
 
