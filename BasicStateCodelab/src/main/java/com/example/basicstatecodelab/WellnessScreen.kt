6fbf280b6bf156dc505fa21ad905da379df1fe9a
Thirawat-r <thirawat4work.info@gmail.com>
2023-06-20 11:28:51 +0700

    Observable MutableList

--- BasicStateCodelab/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
+++ BasicStateCodelab/src/main/java/com/example/basicstatecodelab/WellnessScreen.kt
@@ -2,7 +2,10 @@ package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import com.example.basicstatecodelab.data.WellnessTask
 
 @Composable
 fun WellnessScreen(
@@ -10,7 +13,15 @@ fun WellnessScreen(
 ) {
     Column(modifier = modifier) {
         StatefulCounter()
-        WellnessTasksList()
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(
+            list = list,
+            onCloseTask = { task -> list.remove(task) }
+        )
     }
 }
 
+private fun getWellnessTasks() = List(30) { index ->
+    WellnessTask(index, "Task # $index")
+}
--- BasicStateCodelab/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
+++ BasicStateCodelab/src/main/java/com/example/basicstatecodelab/WellnessTasksList.kt
@@ -3,24 +3,26 @@ package com.example.basicstatecodelab
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.basicstatecodelab.data.WellnessTask
 
 @Composable
 fun WellnessTasksList(
     modifier: Modifier = Modifier,
-    list: List<WellnessTask> = remember { getWellnessTasks() }
+    list: List<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit
 ) {
     LazyColumn(
         modifier = modifier
     ) {
-        items(list) { task ->
-            WellnessTaskItem(taskName = task.label)
+        items(
+            items = list,
+            key = { task -> task.id }
+        ) { task ->
+            WellnessTaskItem(
+                taskName = task.label,
+                onClose = { onCloseTask(task) }
+            )
         }
     }
-}
-
-private fun getWellnessTasks() = List(30) { index ->
-    WellnessTask(index, "Task # $index")
 }
